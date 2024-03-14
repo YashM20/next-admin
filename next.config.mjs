ffce6a7e16ce007b29fac73fe8299c88a2b1c4e1
Yash <yash.mahajan@prometheantech.in>
2024-03-14 15:05:07 +0530

    test

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,21 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd3gga64bj3439.cloudfront.net',
+        pathname: '/Assets/frontend/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'centralci.azureedge.net',
+        pathname: '/livcast-asset/Assets/frontend/**',
+      }
+    ]
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 flex-1 bg-[var(--primary-foreground)] dark">
+    <main className="flex min-h-screen flex-col items-center justify-between  flex-1 bg-[var(--primary-foreground)] dark">
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -38,7 +38,7 @@ export default function Home() {
           height={37}
           priority
         /> */}
-      <CardsStats />
+      {/* <CardsStats /> */}
       {/* </div> */}
 
       {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -113,7 +113,7 @@ export default function Home() {
 
 
 
-      <div className="flex items-center justify-center w-full flex-row space-x-4 bg-slate-500">
+      {/* <div className="flex items-center justify-center w-full flex-row space-x-4 bg-slate-500">
         <div className="grid grid-cols-4 gap-4 dark">
           {
             ["--background", "--foreground", "--card", "--card-foreground", "--popover", "--popover-foreground", "--primary", "--primary-foreground", "--secondary", "--secondary-foreground", "--muted", "--muted-foreground", "--accent", "--accent-foreground", "--destructive", "--destructive-foreground", "--border", "--input", "--ring"].map((color) => (
@@ -134,8 +134,77 @@ export default function Home() {
             ))
           }
         </div>
-      </div>
+      </div> */}
 
+      <section className=" w-full lg:pb-40 h-screen sm:h-screen relative z-[1] bg-[#000] pb-4 overflow-scroll" id="home">
+        <div className=" absolute w-full z-[-2] h-screen md:h-screen overflow-hidden ">
+          <Image
+            src="https://centralci.azureedge.net/livcast-asset/Assets/frontend/bg_livcast.webp"
+            alt="square line"
+            width={1920}
+            height={1080}
+            priority
+            loading="eager"
+            className="w-full h-fit sm:h-fit "
+          />
+          {/* <Image
+            src={"https://d3gga64bj3439.cloudfront.net/Assets/frontend/mobile_event_image.webp"}
+            alt="square line"
+            width={800}
+            height={500}
+            loading='lazy'
+            className="w-full lg:hidden h-fit eventbg"
+          /> */}
+        </div>
+        {/* <Navbar /> */}
+        <div className="md:w-auto flex flex-col lg:flex-row lg:justify-between items-center lg:px-14 md:px-14 xx:px-5 sm:px-[10%] pt-[25px]">
+          <div className="lg:flex-row justify-between items-center sm:h-screen">
+            <div className="section_1 lg:w-11/12">
+              <div className=" md:w-auto maintext1 lg:h-fit sm:h-full lg:text-6xl sm:text-[40px] xx:text-[40px] font-fontpop sm:font-semibold sm:w-[304px]  sm:leading-[44.8px] xx:leading-[44.8px] lg:leading-[78.3px] lg:w-[607px]">
+                Redefining <span>Livestreaming</span>
+              </div>
+              <h5 className="heading md:w-auto mt-5 lg:w-[593px] lg:h-fit text-[16px] font-medium lg:leading-[23.68px] font-fontpop">
+                LivCast revolutionizes streaming with AI-driven customization, superior audio-visual enhancements, and groundbreaking Virtual Live Events, erasing geographical limits for immersive experiences. Experience the future of livestreaming now.
+              </h5>
+              <div className="flex gap-5 xx:gap-1 mt-10">
+                <button
+                  type="button"
+                  className="flex items-center px-6 rounded-full font-semibold text-cp_buttonblack btn1 whitespace-nowrap font-fontpop"
+                // onClick={handlelink}
+                >
+                  Go Live
+                </button>
+                <button
+                  type="button"
+                  className="flex items-center p-1 gap-2 rounded-full font-semibold btn2 text-cp_buttonwhite whitespace-nowrap font-fontpop"
+                >
+                  {/* <Image
+                    src={icons.playbutton}
+                    alt="substract"
+                    width={0}
+                    height={0}
+                  /> */}
+                  <a href="https://youtu.be/XSszKzplJ18">
+                    <span className="sm:mr-8 md:mr-3 lg:mr-2 xx:mr-0">
+                      How it works?
+                    </span>
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="w-full md:pt-6 sm:pt-6 xx:pt-6 xx:items-center xx:ml-auto xx:mr-auto md:w-auto">
+              <Image
+                src={"https://d3gga64bj3439.cloudfront.net/Assets/frontend/livcast-working.svg"}
+                alt="livcastimg"
+                className="w-[312px] md:w-[466px] lg:w-[466px] l"
+                width={0}
+                height={0}
+              />
+            </div> */}
+        </div>
+      </section>
 
     </main>
   );
